846f3c98bbabd47989ff9885d08748c4424c7ffd
szaccagni <sarahzaccagni@gmail.com>
2023-06-08 09:34:08 -0400

    adds minor styling to result

--- src/Result/Result.jsx
+++ src/Result/Result.jsx
@@ -3,17 +3,19 @@ import PieChart from '../PieChart/PieChart'
 export default function Result({zipCodeData}) {
     const usAvg = 49.75
     const backgroundColorResult = zipCodeData.householdFootPrint > usAvg ? 'darkred' : 'darkgreen'
-    const resultsSummary = `${zipCodeData.householdFootPrint > usAvg ? 'higher' : 'lower'} than the united states average of ${usAvg}`
+    const resultsSummary = `Your footprint ${zipCodeData.householdFootPrint > usAvg ? 'higher' : 'lower'} than the united states average`
 
     return (
         <>
         <div className="quiz-result">
             <div className="result-large" style={{backgroundColor: backgroundColorResult}}>
-                <div>
+                <div style={{ height: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end'}}>
                     <div>{zipCodeData.householdFootPrint}</div>
                     <div style={{fontSize: '25px'}}>tons CO2eq/year</div>
                 </div>
-                <div className='result-small-text' style={{color: 'grey'}}>{resultsSummary}</div>
+                <div style={{ height: '40%', display: 'flex', alignItems: 'flex-end'}}>
+                    <div className='result-small-text' style={{color: 'grey'}}>{resultsSummary}</div>
+                </div>
             </div>
             <div className="result-small-top">
                 <PieChart zipCodeData={zipCodeData}/>
