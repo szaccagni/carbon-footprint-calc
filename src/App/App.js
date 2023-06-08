aa2b5dce5fff86f2b4c4a407779885bb92a5fe88
szaccagni <sarahzaccagni@gmail.com>
2023-06-07 21:04:24 -0400

    updates app pg stlying

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Button from '@mui/material/Button';
+import { useState } from 'react'
 
 export default function App() {
+  const [showQuiz, setShowQuiz] = useState(false)
+
   return (
     <div className="App">
-      <div>test</div>
+      {
+        !showQuiz ?
+        <>
+          <div>FIND YOUR FOOTPRINT</div>
+          <Button
+              onClick={() => setShowQuiz(true)}
+              variant="contained"
+          >take the quiz</Button>
+        </>
+        : 
+        <div>quiz</div>
+      }
     </div>
   );
 }
